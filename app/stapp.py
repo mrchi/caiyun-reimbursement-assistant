a20f12bad5f040c73ac1aee596f0e14bcd731da3
mrchi <i@mrchi.cc>
2023-08-10 14:52:15 +0800

    Add requirement openpyxl, update app

--- app/stapp.py
+++ app/stapp.py
@@ -1,5 +1,10 @@
+import pathlib
 from decimal import Decimal
+from io import BytesIO
+from typing import Any
 
+import arrow
+import openpyxl
 import streamlit as st
 
 from app.parse_billing import BillingInfo, BillingParser
@@ -9,6 +14,10 @@ ip = InvoiceParser()
 bp = BillingParser(
     st.secrets.aliyun_ocr.access_key_id, st.secrets.aliyun_ocr.access_key_secret
 )
+reimbursement_application_template = pathlib.Path(
+    st.secrets.reimbursement_application.template_path
+)
+reimbursement_application_submitter = st.secrets.reimbursement_application.submitter
 
 
 @st.cache_data(persist=True)
@@ -51,7 +60,7 @@ def st_input_data() -> tuple[dict[str, InvoiceInfo], dict[str, BillingInfo]]:
 
 def process_data(
     invoice_result: dict[str, InvoiceInfo], billing_result: dict[str, BillingInfo]
-):
+) -> list[dict[str, Any]]:
     invoices = {}
     for filename, invoice_info in invoice_result.items():
         if invoice_info.payment_item not in invoices:
@@ -113,12 +122,43 @@ def process_data(
     return result
 
 
+def generate_reimbursement_application(
+    items: list[dict[str, Any]]
+) -> tuple[BytesIO, str]:
+    workbook = openpyxl.load_workbook(reimbursement_application_template, keep_vba=True)
+    sheet = workbook["日常报销单"]
+    today = arrow.now(tz="Asia/Shanghai")
+
+    sheet["C5"] = reimbursement_application_submitter
+    sheet["F5"] = today.format("YYYY.MM")
+    sheet["C42"] = reimbursement_application_submitter
+    sheet["F42"] = today.format("YYYY.MM.DD")
+
+    for index, item in enumerate(items):
+        row = index + 9
+        sheet[f"B{row}"] = item["service_start"]
+        sheet[f"C{row}"] = "200300"
+        sheet[f"D{row}"] = "软件"
+        sheet[f"E{row}"] = item["payment_item"].capitalize()
+        sheet[f"G{row}"] = item["rmb_amount"]
+
+    fp = BytesIO()
+    workbook.save(fp)
+
+    filename = f"{reimbursement_application_submitter}-{today.format('YYYYMMDD')}.xlsx"
+
+    return fp, filename
+
+
 def main():
     st.set_page_config(layout="wide")
     st.title("彩云报销小助手 V2.0")
     st.write("上传账单 PDF 和信用卡还款截图，自动生成报销单和飞书审批内容。")
 
     invoice_result, billing_result = st_input_data()
+    if not (invoice_result and billing_result):
+        st.info("请上传账单 PDF 和信用卡还款截图。")
+        st.stop()
 
     st.divider()
 
@@ -126,15 +166,22 @@ def main():
     st.write("按 payment_item 分别校验账单中金额和信用卡还款截图中金额匹配。")
 
     try:
-        result = process_data(invoice_result, billing_result)
+        items = process_data(invoice_result, billing_result)
     except ValueError as e:
         st.error(e)
-        return
+        st.stop()
 
     st.success("校验通过，金额匹配无误。")
-    st.table(result)
+    st.balloons()
+    st.table(items)
 
     st.divider()
+
+    st.header("生成彩云报销单")
+
+    fp, filename = generate_reimbursement_application(items)
+    st.success("生成成功！")
+    st.download_button(label="下载报销单", data=fp, file_name=filename)
 
 
 if __name__ == "__main__":
