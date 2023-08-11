94801d326f80cf974fdd95b096a2ecb8e6a72fd2
mrchi <i@mrchi.cc>
2023-08-11 16:43:55 +0800

    Update PaymentItem

--- app/common.py
+++ app/common.py
@@ -2,8 +2,18 @@ from enum import StrEnum
 
 
 class PaymentItem(StrEnum):
-    GITHUB = "github"
-    MAILGUN = "mailgun"
     JIRA = "jira"
-    ONEPASSWD = "1password"
+    MAILGUN = "mailgun"
     AZURE = "azure"
+    GITHUB = "github"
+    ONEPASSWD = "1password"
+
+    @property
+    def description(self):
+        return {
+            self.JIRA: "缺陷跟踪管理系统",
+            self.MAILGUN: "海外邮件发送服务",
+            self.AZURE: "微软 Azure 云服务",
+            self.GITHUB: "GitHub 组织账号",
+            self.ONEPASSWD: "密码管理工具",
+        }[self]
